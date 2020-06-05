0699692427ed6350cc9fa0f73076f233effd3507
Jonas Bighetti <jonasbighetti@yahoo.com.br>
2020-06-04 21:44:12 -0300

    Commit inicial.

---
+++ ProtobufNetExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using ProtoBuf;
+using System.IO;
+
+namespace ProtobufNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                Id = 1,
+                FirstName = "Jonas",
+                LastName = "Bighetti",
+                Age = 29
+            };
+
+            var personSerialize = SerializeObject(person);
+            var personDeserialize = DeserializeObject(personSerialize);
+        }
+
+        private static byte[] SerializeObject(Person obj)
+        {
+            using var stream = new MemoryStream();
+            Serializer.Serialize(stream, obj);
+            return stream.ToArray();
+        }
+
+        private static Person DeserializeObject(byte[] data)
+        {
+            using var stream = new MemoryStream(data);
+            return Serializer.Deserialize<Person>(stream);
+        }
+    }
+
+    [ProtoContract]
+    public class Person
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+        [ProtoMember(2)]
+        public string FirstName { get; set; }
+        [ProtoIgnore]
+        public string LastName { get; set; }
+        [ProtoMember(4)]
+        public int Age { get; set; }
+    }
+}
